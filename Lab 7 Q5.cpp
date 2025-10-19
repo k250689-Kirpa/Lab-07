2858176fd944d464750780db639d213abaa2d5a0
Kirpa Lund <k250689@nu.edu.pk>
2025-10-19 22:57:55 +0500

    Update Lab 7 Q5.cpp

--- Lab 7 Q5.cpp
+++ Lab 7 Q5.cpp
@@ -1,3 +1,11 @@
+
+//Question 5: A shopkeeper is checking the prices of items in his store. He has a list of item prices stored in an array.
+//A customer comes and says:
+//“I want to buy two items whose total price is exactly equal to my budget.”
+//The shopkeeper wants to quickly find which two items have prices that add up to the customer’s given budget (it is
+//also called 2 sum problem). You are required to help him by writing a C program that finds and displays the pair of
+//item prices whose sum equals the given budget.
+	
 #include<stdio.h>
 int main()
 {
@@ -30,3 +38,4 @@ int main()
 	}
 	return 0;
 }
+
