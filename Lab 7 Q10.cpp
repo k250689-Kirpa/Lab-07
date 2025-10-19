c08afaa32abb4efb947e95b339aeac56744f7889
Kirpa Lund <k250689@nu.edu.pk>
2025-10-19 23:04:04 +0500

    Update Lab 7 Q10.cpp

--- Lab 7 Q10.cpp
+++ Lab 7 Q10.cpp
@@ -1,3 +1,9 @@
+//Question 10: A data analyst is testing a search function. The program first generates a random list of 10 integers.
+//The user then enters a number to search. The program should check each element one by one and stop only when
+//the number is found, displaying its position. Write a C program that generates 10 random numbers, takes a search
+//value from the user, and stops when the value is found. Display the index where the number was found.
+
+
 #include <stdio.h>
 int main() {
     int arr[10];
@@ -29,4 +35,5 @@ int main() {
 
     return 0;
 }
+
 
