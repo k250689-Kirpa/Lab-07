99444e81ce4ea2a43847c8eb44469d239b6cb713
Kirpa Lund <k250689@nu.edu.pk>
2025-10-19 23:02:44 +0500

    Update Lab 7 Q9.cpp

--- Lab 7 Q9.cpp
+++ Lab 7 Q9.cpp
@@ -1,3 +1,12 @@
+//Question 9: A travel company maintains an online bus booking system for a bus with 10 seats. The seats are
+//represented by an array where 0 means empty and 1 means booked.
+//When a passenger enters a seat number (0–9):
+//● If the seat is empty, mark it booked and display a confirmation.
+//● If it’s already booked, display “Seat already booked.”
+//After 5 successful bookings, stop the program and display the final seat status
+//Write a C program to simulate the bus seat booking process as described above.
+
+
 #include <stdio.h>
 
 int main() {
@@ -28,4 +37,5 @@ int main() {
     
     return 0;
 }
+
 
