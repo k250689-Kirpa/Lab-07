a8c8610f5157103e43df2e96b47c5b00540ac287
Kirpa Lund <k250689@nu.edu.pk>
2025-10-19 22:50:11 +0500

    Update Lab 7 Q1.cpp

--- Lab 7 Q1.cpp
+++ Lab 7 Q1.cpp
@@ -1,6 +1,7 @@
 //Question 1: A school administrator has a sorted list of student roll numbers stored in an array.
-//Due to a data entry mistake, some roll numbers were entered more than once. Now, the administrator wants to remove all duplicate roll numbers 
-//so that each student appears only once in the list. Write a  program that deletes duplicate elements from the sorted array of roll numbers.
+//Due to a data entry mistake, some roll numbers were entered more than once. Now, the administrator wants
+//to remove all duplicate roll numbers so that each student appears only once in the list. 
+//Write a  program that deletes duplicate elements from the sorted array of roll numbers.
 
 
 #include <stdio.h>
@@ -30,4 +31,5 @@ int main()
     printf("\n");
     return 0;
 }
+
 
